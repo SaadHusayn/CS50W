0ad046fe46c878258525c0674dd5183a32c537a4
SaadHusayn <rsaad5095@gmail.com>
2024-07-30 18:55:54 +0500

    created my first react app

---
+++ lecture 6 user interfaces/react work/my-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Button from './Button' ;
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!!</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ lecture 6 user interfaces/react work/my-app/src/Button.js
@@ -0,0 +1,9 @@
+
+
+function Button(){
+    return (
+        <button>This is a Button</button>
+    );
+}
+
+export default Button;
